7be59b71b4967443e9fc7801208bb692d383a2c1
Daniel <schnolldj@gmail.com>
2019-11-03 03:38:55 -0500

    New file structure for flask app

---
+++ Backend/static/marker.js
@@ -0,0 +1,9 @@
+// // add markers to map
+// geojson.features.forEach(marker => {
+//   // create a HTML element for each feature
+//   let el = document.createElement('div');
+//   el.className = 'marker';
+
+//   // make a marker for each feature and add to the map
+//   new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+// });
