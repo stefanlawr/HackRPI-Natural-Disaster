87b3df890c11f3c143b537c5c1102827f9535659
Stefan Strauch <stefan@singlet-2.dynamic.rpi.edu>
2019-11-03 01:49:27 -0500

    Markers

--- Frontend/js/map.js
+++ Frontend/js/map.js
@@ -1,5 +1,34 @@
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic3N0cmF1YzEiLCJhIjoiY2syaHl6eDNpMWI4azNucDBoaTZuamFnZSJ9.9EjV8zoR-lN2pSXc0VTDKA';
+let latlng;
+
+let geojson = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.032, 38.913]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Washington, D.C.'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-122.414, 37.776]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'San Francisco, California'
+      }
+    }
+  ]
+};
 
 const foo = this.document.querySelector('#map');
 let map = new mapboxgl.Map({
@@ -11,10 +40,38 @@ let map = new mapboxgl.Map({
 
 // Gets lat/lng of mouse cursor
 map.on('mousemove', e => {
-  console.log(`${JSON.stringify(e.point)}\n${JSON.stringify(e.lngLat.wrap())}`);
+  let coords = e.lngLat.wrap();
+  latlng = [coords.lng, coords.lat];
 });
 
 // Adds marker to click
-map.on('click', e => {
-  console.log('click');
+map.on('click', () => {
+  try {
+    // new mapboxgl.Marker(el).setLngLat(latlng).addTo(map);
+    geojson.features.push({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: latlng
+      },
+      properties: {
+        title: 'Mapbox1',
+        description: 'San Francisco, California1'
+      }
+    });
+    console.log(latlng);
+
+    render();
+  } catch {
+    console.log('Pin failed');
+  }
 });
+
+const render = () => {
+  geojson.features.forEach(marker => {
+    let el = document.createElement('div');
+    el.className = 'marker';
+
+    new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+  });
+};
