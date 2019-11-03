666679761740bd6d155779fe29e63cefa81a3b83
Daniel <schnolldj@gmail.com>
2019-11-03 11:46:47 -0500

    Remvoed front end

--- Frontend/js/marker.js
+++
@@ -1,9 +0,0 @@
-// // add markers to map
-// geojson.features.forEach(marker => {
-//   // create a HTML element for each feature
-//   let el = document.createElement('div');
-//   el.className = 'marker';
-
-//   // make a marker for each feature and add to the map
-//   new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
-// });
